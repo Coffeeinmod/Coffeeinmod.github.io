c6f0a8e94fc01c121924cc7a5abc3598e850cfa3
Coffeeinmod <Coffeeinmod@users.noreply.github.com>
2026-09-10 10:33:51 +0000

    Publish clean static CoffeeinMod site for GitHub Pages

--- src/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { Landing } from "@/components/coffeein/landing";
-
-export const Route = createFileRoute("/")({ component: Home });
-
-function Home() {
-  return <Landing />;
-}
